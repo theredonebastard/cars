b84391eb16f0eb2799b5ea018df2239f9efd9d85
Johnny Farrel <theredonebastard@gmail.com>
2019-02-25 09:48:59 +0200

    Append tax attribute to Car model

--- cars/app/Car.php
+++ cars/app/Car.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Car extends Model
 {
-	public function tax() {
+	protected $appends = ['tax'];
+
+	public function getTaxAttribute() {
 		return ((int)date('Y') - (int)$this->year) * $this->engine_displacement * 100;
 	}
 }
