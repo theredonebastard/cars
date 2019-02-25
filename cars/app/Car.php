637b4ac4f34878f43a1aca5c94e2dd0d53123c6b
Johnny Farrel <theredonebastard@gmail.com>
2019-02-25 16:58:04 +0200

    Add fillable fields

--- cars/app/Car.php
+++ cars/app/Car.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Car extends Model
 {
 	protected $appends = ['tax'];
+	protected $fillable = ['title', 'color', 'year', 'make', 'model', 'body_style', 'engine_displacement'];
 
 	public function getTaxAttribute() {
 		return ((int)date('Y') - (int)$this->year) * $this->engine_displacement * 100;
